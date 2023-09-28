5043826485daa5127799fd36967b8be9545b70a5
Lisa S <halisasaipulla@gmail.com>
2023-09-28 10:41:37 -0700

    displayed a button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
     return (
-        <p className='text-3xl font-bold text-indigo-500'>
-            Hello Discord!
-        </p>
+        <div>
+            <p className='text-3xl font-bold text-indigo-500'>
+                Hello Discord!
+            </p>
+            <Button>
+                click me
+            </Button>
+        </div>
+        
     )
 }
