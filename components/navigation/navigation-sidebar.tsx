4924d878c4c2a2de62ec7409c89c91bf5bc3df6b
Lisa S <halisasaipulla@gmail.com>
2023-10-07 10:39:19 -0700

    created inital navigation-item

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation";
 import NavigationAction from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
 
 const NavigationSidebar = async() => {
     const profile = await currentProfile();
@@ -30,7 +31,11 @@ const NavigationSidebar = async() => {
             <ScrollArea className="flex-1 w-full">
                 {servers.map((server) => (
                     <div key={server.id} className="mb-4">
-                        {server.name}
+                        <NavigationItem
+                            id={server.id}
+                            name={server.name}
+                            imageUrl={server.imageUrl}
+                        />
                     </div>
                 ))}
             </ScrollArea>
