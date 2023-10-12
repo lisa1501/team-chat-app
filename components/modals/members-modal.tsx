8859a58b2eb198d6d9fa081705574e006d14076a
Lisa S <halisasaipulla@gmail.com>
2023-10-12 10:49:03 -0700

    rendered members' avatar,role and email

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -10,6 +10,15 @@ import {
 
 import { useModal } from "@/hooks/use-modal-store";
 import { ServerWithMembersWithProfiles } from "@/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { UserAvatar } from "@/components/user-avatar";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+
+const roleIconMap = {
+    "GUEST": null,
+    "MODERATOR": <ShieldCheck className="h-4 w-4 ml-2 text-indigo-500" />,
+    "ADMIN": <ShieldAlert className="h-4 w-4 text-rose-500" />
+}
 
 const MembersModal = () => {
     const {onOpen, isOpen, onClose, type, data } = useModal();
@@ -29,9 +38,22 @@ const MembersModal = () => {
                         {server?.members?.length} Members
                     </DialogDescription>
                 </DialogHeader>
-                <div className="p-6">
-                    Hello Members
-                </div>
+                <ScrollArea className="mt-8 max-h-[420px] pr-6">
+                    {server?.members?.map((member) => (
+                        <div key={member.id} className="flex items-center gap-x-2 mb-6">
+                            <UserAvatar src={member.profile.imageUrl} />
+                            <div className="flex flex-col gap-y-1">
+                                <div className="text-xs font-semibold flex items-center gap-x-1">
+                                    {member.profile.name}
+                                    {roleIconMap[member.role]}
+                                </div>
+                                <p className="text-xs text-zinc-500">
+                                    {member.profile.email}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </ScrollArea>
             </DialogContent>
         </Dialog>
     );
