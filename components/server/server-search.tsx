9184fe4a619afc14e56df34df549efaac8364edc
Lisa S <halisasaipulla@gmail.com>
2023-10-17 11:12:37 -0700

    implemneted keyboard shortcut for search server feature

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Search } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 
 interface ServerSearchProps {
@@ -20,6 +20,18 @@ export const ServerSearch = ({
     }: ServerSearchProps) =>{
 
         const [open, setOpen] = useState(false);
+
+        useEffect(() => {
+            const down = (e: KeyboardEvent) => {
+                if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+                    e.preventDefault();
+                    setOpen((open) => !open);
+                }
+            }
+        
+            document.addEventListener("keydown", down);
+            return () => document.removeEventListener("keydown", down)
+        }, []);
         
         return (
             <>
