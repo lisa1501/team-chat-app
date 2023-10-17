b5c8b0be7cf4ccf138302496e8c0569cffee5a9a
Lisa S <halisasaipulla@gmail.com>
2023-10-16 21:40:27 -0700

    added some attributes to server search component

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,9 +1,21 @@
 "use client";
-
-export const ServerSearch = () =>{
-    return (
-        <div>
-            Server Search component !
-        </div>
-    )
+interface ServerSearchProps {
+    data: {
+        label: string;
+        type: "channel" | "member",
+        data: {
+            icon: React.ReactNode;
+            name: string;
+            id: string;
+        }[] | undefined
+    }[]
 }
+export const ServerSearch = ({
+        data
+    }: ServerSearchProps) =>{
+        return (
+            <div>
+                Server Search component !
+            </div>
+        )
+    }   
