1f90fc417ec37e7a7f4bb87676cb710c283988a9
Lisa S <halisasaipulla@gmail.com>
2023-10-26 13:48:00 -0700

    crated interface for chatheader component

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,6 +1,19 @@
-export const ChatHeader =() => {
+
+interface ChatHeaderProps {
+    serverId: string;
+    name: string;
+    type: "channel" | "conversation";
+    imageUrl?: string;
+}
+
+export const ChatHeader =({
+    serverId,
+    name,
+    type,
+    imageUrl
+}: ChatHeaderProps) => {
     return (
-        <div>
+        <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
             ChatHeader componnet !
         </div>
     )
