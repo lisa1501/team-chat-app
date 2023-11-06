3321f9363476ff7de5e8f5a6e0ab33c127dc944b
Lisa S <halisasaipulla@gmail.com>
2023-11-06 14:17:18 -0800

    rendered message timestamp

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -41,19 +41,22 @@ export const ChatItem = ({
     return (
         <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
             <div className="group flex gap-x-2 items-start w-full">
-            <div className="cursor-pointer hover:drop-shadow-md transition">
-                <UserAvatar src={member.profile.imageUrl} />
-            </div>
-            <div className="flex flex-col w-full">
-                <div className="flex items-center">
-                    <p className="font-semibold text-sm hover:underline cursor-pointer">
-                        {member.profile.name}
-                    </p>
-                    <ActionTooltip label={member.role}>
-                        {roleIconMap[member.role]}
-                    </ActionTooltip>
+                <div className="cursor-pointer hover:drop-shadow-md transition">
+                    <UserAvatar src={member.profile.imageUrl} />
                 </div>
-            </div>
+                <div className="flex flex-col w-full">
+                    <div className="flex items-center">
+                        <p className="font-semibold text-sm hover:underline cursor-pointer">
+                            {member.profile.name}
+                        </p>
+                        <ActionTooltip label={member.role}>
+                            {roleIconMap[member.role]}
+                        </ActionTooltip>
+                    </div>
+                    <span className="text-xs text-zinc-500 dark:text-zinc-400">
+                        {timestamp}
+                    </span>
+                </div>
             </div>
         </div>
     )
