f2bfc60fd039724517a87a111ecd42abba946143
Lisa S <halisasaipulla@gmail.com>
2023-11-06 13:57:06 -0800

    styled chat item component

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { Member, Profile } from "@prisma/client";
+import { UserAvatar } from "../user-avatar";
+import { ActionTooltip } from "@/components/action-tooltip";
 
 interface ChatItemProps {
     id: string;
@@ -30,8 +32,22 @@ export const ChatItem = ({
     socketQuery
 }: ChatItemProps)=>{
     return (
-        <div>
-            Chat Item !
+        <div className="relative group flex items-center hover:bg-black/5 p-4 transition w-full">
+            <div className="group flex gap-x-2 items-start w-full">
+            <div className="cursor-pointer hover:drop-shadow-md transition">
+                <UserAvatar src={member.profile.imageUrl} />
+            </div>
+            <div className="flex flex-col w-full">
+                <div className="flex items-center">
+                    <p className="font-semibold text-sm hover:underline cursor-pointer">
+                        {member.profile.name}
+                    </p>
+                    <ActionTooltip label={member.role}>
+                        <p>-Role</p>
+                    </ActionTooltip>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
