d467ff1b155a0d84ec456c0329f0cb6e57774ff0
Lisa S <halisasaipulla@gmail.com>
2023-11-07 12:16:07 -0800

    rendered text message edit form

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -1,12 +1,23 @@
 "use client";
 
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Member, MemberRole, Profile } from "@prisma/client";
 import { UserAvatar } from "../user-avatar";
 import { ActionTooltip } from "@/components/action-tooltip";
 import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 interface ChatItemProps {
     id: string;
@@ -29,6 +40,10 @@ const roleIconMap = {
     "ADMIN": <ShieldAlert className="h-4 w-4 ml-2 text-rose-500" />,
 }
 
+const formSchema = z.object({
+    content: z.string().min(1),
+});
+
 export const ChatItem = ({
     id,
     content,
@@ -42,6 +57,23 @@ export const ChatItem = ({
     socketQuery
 }: ChatItemProps)=>{
     const [isEditing, setIsEditing] = useState(false);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            content: content
+        }
+    });
+
+    const onSubmit = (values) =>{
+        console.log(values)
+    }
+
+    useEffect(() => {
+        form.reset({
+            content: content,
+        })
+    }, [content]);
     
     const fileType = fileUrl?.split(".").pop();
 
@@ -117,6 +149,39 @@ export const ChatItem = ({
                                 </span>
                             )}
                         </p>
+                    )}
+
+            {!fileUrl && isEditing && (
+                        <Form {...form}>
+                            <form 
+                                className="flex items-center w-full gap-x-2 pt-2"
+                                onSubmit={form.handleSubmit(onSubmit)}
+                            >
+                                <FormField
+                                    control={form.control}
+                                    name="content"
+                                    render={({ field }) => (
+                                        <FormItem className="flex-1">
+                                            <FormControl>
+                                                <div className="relative w-full">
+                                                    <Input
+                                                        className="p-2 bg-zinc-200/90 dark:bg-zinc-700/75 border-none border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-zinc-600 dark:text-zinc-200"
+                                                        placeholder="Edited message"
+                                                        {...field}
+                                                    />
+                                                </div>
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button  size="sm" variant="primary">
+                                    Save
+                                </Button>
+                            </form>
+                            <span className="text-[10px] mt-1 text-zinc-400">
+                                Press escape to cancel, enter to save
+                            </span>
+                        </Form>
                     )}
                     
                 </div>    
