839e0f1e7bcbb6cb15956d1d3591e4ee555bc5d7
Lisa S <halisasaipulla@gmail.com>
2023-11-09 17:40:11 -0800

    created use-chat-scroll hook

--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -8,7 +8,7 @@ import { Fragment, useRef, ElementRef } from "react";
 import { ChatItem } from "./chat-item";
 import { format } from "date-fns";
 import { useChatSocket } from "@/hooks/use-chat-socket";
-
+import { useChatScroll } from "@/hooks/use-chat-scroll";
 
 interface ChatMessagesProps {
     name: string;
@@ -62,6 +62,14 @@ export const ChatMessages = ({
         paramValue,
     });
     useChatSocket({ queryKey, addKey, updateKey });
+    
+    useChatScroll({
+        chatRef,
+        bottomRef,
+        loadMore: fetchNextPage,
+        shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+        count: data?.pages?.[0]?.items?.length ?? 0,
+    })
 
     if (status === "loading") {
         return (
